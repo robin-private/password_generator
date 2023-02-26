99cf9ae0ab35f8c6670c2b4346bef438df1ec306
robin-private <robin.info@email.com>
2023-02-26 15:56:04 +0530

    commit initial

--- generator/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.index),
-    path('generated_password/', views.password, name='password'),
-    path('about/', views.about, name='about')
-
-]
--- generator/views.py
+++ generator/views.py
@@ -1,29 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-import random
-
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
-
-
-def password(request):
-    characters = list('abcdefghijklmnopqrstuvwxyz')
-    if request.GET.get('uppercase'):
-        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
-    if request.GET.get('numbers'):
-        characters.extend(list('0123456789'))
-    if request.GET.get('special'):
-        characters.extend(list('!@#$%^&*'))
-    # print(characters)
-    length = int(request.GET.get('length', 14))
-    # print(length)
-    generated_password = ''
-    for x in range(length):
-        generated_password += random.choice(characters)
-    return render(request, 'index.html', {'password': generated_password})
-
-
-def about(request):
-    return render(request, 'about.html')
